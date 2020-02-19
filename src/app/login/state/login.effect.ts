627f0a5bbafbfe3f88da4ca9f24bcfb503d94928
Matheus Pitz <matheushenriquepitz@gmail.com>
2020-02-18 22:48:04 -0300

    Fixed effect problem.

--- src/app/login/state/login.effect.ts
+++ src/app/login/state/login.effect.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { map, switchMap, catchError } from 'rxjs/operators';
+import { map, switchMap, catchError, tap } from 'rxjs/operators';
 
 import { ActionTypes, SuccessfullyLogged, Login, LoginError, InvalidLogin } from './login.actions'
 import { LoginService } from '../login.service';
 import { AuthenticationToken } from '../login.model';
-import { of, Observable, empty } from 'rxjs';
+import { of, Observable, empty, never } from 'rxjs';
 import { TokenService } from 'src/app/token/token.service';
 
 @Injectable({
@@ -16,24 +16,23 @@ export class LoginEffects {
     @Effect()
     login$ = this.actions$.pipe(ofType(ActionTypes.Login), switchMap((action: Login) => {               
         return this.service.authenticate(action.payload).pipe(map((authToken: AuthenticationToken) => {
-            if(authToken != null && authToken.token != null && authToken.token.length > 0)
+            if(authToken != null && authToken.token != null && authToken.token.length > 0) {                
                 return new SuccessfullyLogged(authToken);
-            else 
+            } else { 
                 return new InvalidLogin();
+            }
         }), catchError(() => of(new LoginError())));
-    }));
+    })); 
+    
+    @Effect({
+        dispatch: false
+    })
+    successfullyLogged$ = this.actions$.pipe(ofType(ActionTypes.SuccessfullyLogged), tap((action: SuccessfullyLogged) => this.tokenService.setToken(action.payload.token)));
 
-    @Effect()
-    successfullyLogged$ = this.actions$.pipe(ofType(ActionTypes.SuccessfullyLogged), switchMap((action: SuccessfullyLogged) => {
-        this.tokenService.setToken(action.payload.token);
-        return empty;
-    }));
-
-    @Effect()
-    logout$ = this.actions$.pipe(ofType(ActionTypes.Logout), switchMap(() => {
-        this.tokenService.deleteToken();
-        return empty;
-    }));
+    @Effect({
+        dispatch: false
+    })
+    logout$ = this.actions$.pipe(ofType(ActionTypes.Logout), tap(() => this.tokenService.deleteToken()));
 
     constructor(
         private actions$: Actions,
